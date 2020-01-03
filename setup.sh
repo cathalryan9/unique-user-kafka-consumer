f4febdca28a0da4919fd126e7bf530a4e009b57f
Cathal Ryan <cathalryan92@gmail.com>
2020-01-02 23:01:39 -0500

    Add mvn install to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt-get install maven
 ./kafka_2.*/bin/kafka-server-start.sh kafka_2.*/config/server.properties |
 ./kafka_2.*/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic cathalryantest |
 # Start app
+mvn clean install
 mvn exec:java
 # Input data
 #./kafka_2.*/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic cathalryantest < ../stream.jsonl
